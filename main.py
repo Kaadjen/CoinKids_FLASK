f854b6a40bc54337c4ed6a9d3f3b64155a99577d
Kaadjen <91345537+Kaadjen@users.noreply.github.com>
2024-01-10 16:41:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -589,5 +589,7 @@ def schedule_money_addition():
 # RUN
 thread = threading.Thread(target=schedule_money_addition)
 thread.start()
-webbrowser.open('http://127.0.0.1:5000/')
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 app.run(debug=True, use_reloader=False)
